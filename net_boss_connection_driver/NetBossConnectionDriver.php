4c8fc3611fed6939cb0452df805ad43fea618dc8
Kolade Ige <Kolade.Ige@mainonecable.com>
2017-11-22 07:44:35 +0100

    CLEANING UP THE SLATE

--- net_boss_connection_driver/NetBossConnectionDriver.php
+++ net_boss_connection_driver/NetBossConnectionDriver.php
@@ -31,26 +31,65 @@ class NetBossConnectionDriver extends MiddlewareConnectionDriver {
         // Get a connection token
         return $this->getDevices();
     }
-
+    
     private function getDevices(){      
-        $url = 'http://nbportalproxy.mainone.net/echo.php';
-        $data = ['method' => 'authenticate', 'parameters' => serialize(["api", "mumu&711"])];
         
-        // use key 'http' even if you send the request to https://...
-        $options = array(
-            'http' => array(
-                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-                'method'  => 'POST',
-                'content' => http_build_query($data)
-            )
-        );
-
-        $context  = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
-        if ($result === FALSE) { /* Handle error */ }
+        // ini_set ( "soap.wsdl_cache_enabled", 1 );
+        ini_set('soap.wsdl_cache_enabled',0);
+        ini_set('soap.wsdl_cache_ttl',0);
         
-        var_dump($result);
+        $soapUrl = "https://middleware-dev.mainone.net/net-boss/wsdl/1";
+        
+        $context = stream_context_create ([ 
+                'ssl' => [ 
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true 
+                ] 
+        ]);
+        
+        $client = new \SoapClient ( $soapUrl, array (
+                //'trace' => 1,
+                'sream_context' => $context 
+        ));
+        
+        if (!$client) {
+            echo ("Could not connect to SOAP server\n");
+        }
+        
+        try {
+            $result = $client->authenticate( "appdev", "nbportal" );
+            if (! $result) {
+                return ("Could not authenticate with the server\n");
+            } else {
+                $result = $client->getAuthenticatedUid();
+                $devices = $client->report_getReportAttachmentsByReportId(43);
+                return $devices;
+            }
+        } catch ( \Exception $e ) {
+            print_r ( $e );
+        }
     }
+    
+    // private function getDevices(){      
+    //     $url = 'https://itsupport.mainone.net/netboss/echo.php';
+    //     $data = ['method' => 'authenticate', 'parameters' => serialize(["appdev", "nbportal"])];
+        
+    //     // use key 'http' even if you send the request to https://...
+    //     $options = array(
+    //         'http' => array(
+    //             'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+    //             'method'  => 'POST',
+    //             'content' => http_build_query($data)
+    //         )
+    //     );
+
+    //     $context  = stream_context_create($options);
+    //     $result = file_get_contents($url, false, $context);
+    //     if ($result === FALSE) { /* Handle error */ }
+        
+    //     return $result;
+    // }
 
     public function getStringer() {
         return MiddlewareFilter::SQL;
