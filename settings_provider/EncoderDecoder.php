eeeaa2c4c69201e2a724066a88ebf0141ef86d66
Kolade Ige <Kolade.Ige@mainonecable.com>
2018-03-27 18:11:35 +0100

    ADDED SUPPORT FOR CURLY BRACES IN ODATA FILTER QUERY

--- settings_provider/EncoderDecoder.php
+++ settings_provider/EncoderDecoder.php
@@ -4,14 +4,14 @@ namespace com\mainone\middleware;
 
 class EncoderDecoder{
     public static function escape($value){
-        return str_replace(['\\(', '\\)', "\\'"], ['_y0028_','_y0029_', '_y0027_'], $value);
+        return str_replace(['\\(', '\\)', "\\'",'\\{', '\\}'], ['_y0028_','_y0029_', '_y0027_', '_y007B_','_y007D_'], $value);
     }
 
     public static function unescape($value, $niddle = [], $replacement = [], $enforce = FALSE){
         if($enforce){
             return str_replace($niddle, $replacement, $value);
         } else {
-            return str_replace(array_merge(['_y0028_','_y0029_', '_y0027_'], $niddle), array_merge(['(', ')', "\'"], $replacement), $value);
+            return str_replace(array_merge(['_y0028_','_y0029_', '_y0027_', '_y007B_','_y007D_'], $niddle), array_merge(['(', ')', "\'", '{', '}'], $replacement), $value);
         }
     }
 
@@ -19,16 +19,16 @@ class EncoderDecoder{
         if($enforce){
             return str_replace($niddle, $replacement, $value);
         } else {
-            return str_replace(array_merge(['(', ')', "'"], $niddle), array_merge(['_y0028_','_y0029_', '_y0027_'], $replacement), $value);
+            return str_replace(array_merge(['(', ')', "'", '{', '}'], $niddle), array_merge(['_y0028_','_y0029_', '_y0027_', '_y007B_','_y007D_'], $replacement), $value);
         }
     }
 
     public static function encode($value){
-        return str_replace(['(', ')', "'"], ['_x0028_','_x0029_', '_x0027_'], $value);
+        return str_replace(['(', ')', "'", '{', '}'], ['_x0028_','_x0029_', '_x0027_', '_y007B_','_y007D_'], $value);
     }
 
     public static function decode($value){
-        return str_replace(['_x0028_','_x0029_', '_x0027_'], ['(', ')', "'"], $value);
+        return str_replace(['_x0028_','_x0029_', '_x0027_', '_y007B_','_y007D_'], ['(', ')', "'", '{', '}'], $value);
     }
 
     public static function unescapeall($value){
